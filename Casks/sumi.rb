739063b8bee126827f62b2ef26087bd1bf66daeb
Yu Lun Hsu <alan890104@gmail.com>
2026-03-09 00:50:06 +0800

    cask: bump sumi to 0.5.2

--- Casks/sumi.rb
+++ Casks/sumi.rb
@@ -1,10 +1,10 @@
 cask "sumi" do
-  version "0.3.0"
-  sha256 "d5f53967c9411596aaf58d80c07a265555aa0ced85a077cf597fcbc00aefb3ec"
+  version "0.5.2"
+  sha256 "4b960cb149cb95a68a4d9bd084edfc902f25bac7eff57d1af1c328dd8c5c2f6c"
 
   url "https://github.com/alan890104/sumi/releases/download/v#{version}/Sumi_#{version}_aarch64.dmg"
   name "Sumi"
-  desc "System-wide speech-to-text transcription app"
+  desc "System-wide speech-to-text with AI polish for macOS"
   homepage "https://github.com/alan890104/sumi"
 
   depends_on arch: :arm64
